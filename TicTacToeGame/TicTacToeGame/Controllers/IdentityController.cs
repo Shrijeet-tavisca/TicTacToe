e8850b2772cfdaf92d97854a3b0e515e9fcee497
Shrijeet Karade <41461751+Shrijeet-tavisca@users.noreply.github.com>
2018-09-22 12:05:27 +0530

    Removed Commented Code

--- TicTacToeGame/TicTacToeGame/Controllers/IdentityController.cs
+++ TicTacToeGame/TicTacToeGame/Controllers/IdentityController.cs
@@ -1,96 +1,4 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
 
-//namespace TicTacToeGame.Controllers
-//{
-//    public class IdentityController : Controller
-//    {
-//        // GET: Identity
-//        public ActionResult Index()
-//        {
-//            return View();
-//        }
-
-//        // GET: Identity/Details/5
-//        public ActionResult Details(int id)
-//        {
-//            return View();
-//        }
-
-//        // GET: Identity/Create
-//        public ActionResult Create()
-//        {
-//            return View();
-//        }
-
-//        // POST: Identity/Create
-//        [HttpPost]
-//        [ValidateAntiForgeryToken]
-//        public ActionResult Create(IFormCollection collection)
-//        {
-//            try
-//            {
-//                // TODO: Add insert logic here
-
-//                return RedirectToAction(nameof(Index));
-//            }
-//            catch
-//            {
-//                return View();
-//            }
-//        }
-
-//        // GET: Identity/Edit/5
-//        public ActionResult Edit(int id)
-//        {
-//            return View();
-//        }
-
-//        // POST: Identity/Edit/5
-//        [HttpPost]
-//        [ValidateAntiForgeryToken]
-//        public ActionResult Edit(int id, IFormCollection collection)
-//        {
-//            try
-//            {
-//                // TODO: Add update logic here
-
-//                return RedirectToAction(nameof(Index));
-//            }
-//            catch
-//            {
-//                return View();
-//            }
-//        }
-
-//        // GET: Identity/Delete/5
-//        public ActionResult Delete(int id)
-//        {
-//            return View();
-//        }
-
-//        // POST: Identity/Delete/5
-//        [HttpPost]
-//        [ValidateAntiForgeryToken]
-//        public ActionResult Delete(int id, IFormCollection collection)
-//        {
-//            try
-//            {
-//                // TODO: Add delete logic here
-
-//                return RedirectToAction(nameof(Index));
-//            }
-//            catch
-//            {
-//                return View();
-//            }
-//        }
-//    }
-//}
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -125,8 +33,7 @@ namespace TicTacToeGame
                 return Ok("Player Added successfully Your Name is "+player.Name+"& Your ID is"+ player.Id +"");
             return BadRequest("Could not add Player");
         }
-
-
+        
         // PUT: api/Identity/5
         [HttpPut("{id}")]
         [Logger]
